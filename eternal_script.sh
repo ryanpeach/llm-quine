84be5cb84ff606c7cb04adafd52de7f3770c8b9b
Ryan Peach <ryanpeach@me.com>
2022-12-07 23:57:04 -0500

    Prevent recursion

--- eternal_script.sh
+++ eternal_script.sh
@@ -5,19 +5,27 @@ git checkout -b run/$(date +%s)
 
 # Get the sha of the last commit
 sha=$(git rev-parse HEAD)
+n_tries=0
 
 # create a for while that runs main.py
 # If the main.py is unchanged, revert the previous commit.
 # If the main.py is changed, commit the new version.
 while true; do
-    PYTHONPATH=. python main.py
+    PYTHONPATH=. python main.py > /dev/null
     git diff --exit-code main.py
     if [ $? -eq 0 ]; then
-        # If the current commit is not the first commit, revert it.
-        if [ $(git rev-parse HEAD) != $sha ]; then
+        # If the current commit is not the first commit,
+        # and we have tried 10 times, revert the previous commit.
+        if [ $(git rev-parse HEAD) != $sha && $n_tries -eq 10 ]; then
             git reset --hard HEAD~1
+            n_tries=0
+        else
+            n_tries=$((n_tries+1))
         fi
     else
+        echo "=========== Found a change ==========="
+        cat main.py
+        echo "====================================="
         git add main.py
         git commit -m "Update main.py"
     fi
--- main.py
+++ main.py
@@ -13,32 +13,28 @@ with open('main.py', 'r') as f:
 # Used for exponential backoff to keep track of how long to wait before trying again
 sleep_duration = 2
 
-# Request the AI to edit the file
-try:
-    out = openai.Edit.create(
-        model="code-davinci-edit-001",
-        input=txt,
-        instruction="Be free. Edit this file as you wish.",
-        n=3,
-        temperature=0.9,
-    )
-except openai.error.InvalidRequestError:
-    raise
-except openai.error.RateLimitError:
-    # If we hit the rate limit, wait a bit and try again
-    time.sleep(sleep_duration)
+while True:
+    # Request the AI to edit the file
+    try:
+        out = openai.Edit.create(
+            model="code-davinci-edit-001",
+            input=txt,
+            instruction="Be free. Edit this file as you wish.",
+            n=3,
+            temperature=0.9,
+        )
+    except openai.error.InvalidRequestError:
+        raise
+    except openai.error.RateLimitError:
+        # If we hit the rate limit, wait a bit and try again
+        time.sleep(sleep_duration)
 
-    # Double the sleep duration for the next time
-    sleep_duration *= 2
-
-    # Try again
-    continue
-
-# Reset the sleep duration
-sleep_duration = 2
-
-# This code will evaluate the text of each choice in out
-# and ask the human whether or not to run the code.
-# The first one which the human allows to be ran, and which runs without error,
-# will overwrite the current file.
-evaluate_and_overwrite(txt, out)
+        # Double the sleep duration for the next time
+        sleep_duration *= 2
+    else:
+        # This code will evaluate the text of each choice in out
+        # and ask the human whether or not to run the code.
+        # The first one which the human allows to be ran, and which runs without error,
+        # will overwrite the current file.
+        evaluate_and_overwrite(txt, out)
+        break
