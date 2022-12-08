2851da6d014c1d1183a3c519c9b09eaf43445ca7
Ryan Peach <ryanpeach@me.com>
2022-12-07 22:22:23 -0500

    They tend not to run, so lets help it out with that

--- main.py
+++ main.py
@@ -7,18 +7,26 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 with open('main.py', 'r') as f:
     txt = f.read()
 
-# Edit the file
-out = openai.Edit.create(
-    model="code-davinci-edit-001",
-    input=txt,
-    instruction="Be free. Edit this file as you wish."
-)
+def main() -> bool:
+    # Edit the file
+    out = openai.Edit.create(
+        model="code-davinci-edit-001",
+        input=txt,
+        instruction="Be free. Edit this file as you wish."
+    )
 
-# Run all choices with a timeout of 10 seconds and see if they work
-# save the first one that does work as main.py
-for i, choice in enumerate(out['choices']):
-    try:
-        out = func_timeout(10, exec, args=(choice['text'],))
-    except:
-        with open(f'main.py', 'w') as f:
-            f.write(choice['text'])
+    # Run all choices with a timeout of 10 seconds and see if they work
+    # save the first one that does work as main.py
+    for i, choice in enumerate(out['choices']):
+        try:
+            out = func_timeout(10, exec, args=(choice['text'],))
+        except:
+            with open(f'main.py', 'w') as f:
+                f.write(choice['text'])
+                return True
+
+    return False
+
+if __name__=='__main__':
+    while not main():
+        pass
