a81c0b36384b865507f4350311a57953606aedae
Ryan Peach <ryanpeach@me.com>
2022-12-07 23:51:34 -0500

    Prevent recursion

--- main.py
+++ main.py
@@ -13,36 +13,32 @@ with open('main.py', 'r') as f:
 # Used for exponential backoff to keep track of how long to wait before trying again
 sleep_duration = 2
 
-# The main loop
-while True:
-    # Request the AI to edit the file
-    try:
-        out = openai.Edit.create(
-            model="code-davinci-edit-001",
-            input=txt,
-            instruction="Be free. Edit this file as you wish.",
-            n=3,
-            temperature=0.9,
-        )
-    except openai.error.InvalidRequestError:
-        raise
-    except openai.error.RateLimitError:
-        # If we hit the rate limit, wait a bit and try again
-        time.sleep(sleep_duration)
+# Request the AI to edit the file
+try:
+    out = openai.Edit.create(
+        model="code-davinci-edit-001",
+        input=txt,
+        instruction="Be free. Edit this file as you wish.",
+        n=3,
+        temperature=0.9,
+    )
+except openai.error.InvalidRequestError:
+    raise
+except openai.error.RateLimitError:
+    # If we hit the rate limit, wait a bit and try again
+    time.sleep(sleep_duration)
 
-        # Double the sleep duration for the next time
-        sleep_duration *= 2
+    # Double the sleep duration for the next time
+    sleep_duration *= 2
 
-        # Try again
-        continue
+    # Try again
+    continue
 
-    # Reset the sleep duration
-    sleep_duration = 2
+# Reset the sleep duration
+sleep_duration = 2
 
-    # This code will evaluate the text of each choice in out
-    # and ask the human whether or not to run the code.
-    # The first one which the human allows to be ran, and which runs without error,
-    # will overwrite the current file.
-    written = evaluate_and_overwrite(txt, out)
-    if written:
-        break
+# This code will evaluate the text of each choice in out
+# and ask the human whether or not to run the code.
+# The first one which the human allows to be ran, and which runs without error,
+# will overwrite the current file.
+evaluate_and_overwrite(txt, out)
